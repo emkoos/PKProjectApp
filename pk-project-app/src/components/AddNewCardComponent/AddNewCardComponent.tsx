946ad658941589bcd358bb5472cd1ab4709ccb9a
emil.saracyn <emil.saracyn@gmail.com>
2022-01-08 00:31:35 +0100

    Datepicker first config

--- pk-project-app/src/components/AddNewCardComponent/AddNewCardComponent.tsx
+++ pk-project-app/src/components/AddNewCardComponent/AddNewCardComponent.tsx
@@ -1,19 +1,43 @@
 import { Formik } from "formik"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap"
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router"
+import { createCard } from "../../api/cards";
+import { IColumn, IState } from "../../state";
+import { setColumns } from "../../state/columnCards/action";
 import { IForm } from "./constants";
 
 
 const AddNewCardComponent = () => {
     const navigate = useNavigate();
+    const [columnId, setColumnId] = useState<string>();
+    const column = useSelector<IState, IColumn>((state) => state.column);
 
     const [initialValues, setInitialValues] = useState<IForm>({
         localisation: 3
     })
 
+    useEffect(() => {
+        setColumnId(column.id);
+    }, [])
+
     const submitHandler = (values: any, handlers: any) => {
-        navigate('/my-scrum');
+        console.log(values.title);
+        console.log(values.description);
+        console.log(values.email);
+        console.log(columnId);
+        console.log(values.status);
+        console.log(new Date().toString());
+        console.log(values.priority);
+        console.log(values.estimate);
+
+        createCard(values.title, values.description, values.email, columnId, values.status, "18.11.2021 20:30:03", values.priority, values.estimate, "2")
+            .then(() => {
+                navigate('/my-scrum');
+            }).catch(error => {
+                console.log(error);
+            });
     };
 
     return (
@@ -29,13 +53,31 @@ const AddNewCardComponent = () => {
                     {({handleSubmit, handleChange, handleBlur, values, touched, errors}) => (
                         <Form onSubmit={handleSubmit}>
                             <Row className="mt-3">
-                                <Form.Label className="w-100 text-start px-0">Lokalizacja</Form.Label>
-                                <Form.Select name="localisation" className="w-100 text-start px-0 ps-3" value={values.localisation} onChange={handleChange}>
-                                    <option value={"elo"}>elos</option>
-                                    <option value={"elo1"}>elos1</option>
-                                    <option value={"elo2"}>elos2</option>
-                                </Form.Select>
+                                <Form.Label className="w-100 text-start px-0">Tytuł</Form.Label>
+                                <Form.Control type="textarea" name="title" className="w-100 text-start px-0 ps-3" onChange={handleChange} />
                             </Row>
+                            <Row className="mt-3">
+                                <Form.Label className="w-100 text-start px-0">Opis</Form.Label>
+                                <Form.Control type="textarea" name="description" className="w-100 text-start px-0 ps-3" onChange={handleChange} />
+                            </Row>
+                            <Row className="mt-3">
+                                <Form.Label className="w-100 text-start px-0">Email</Form.Label>
+                                <Form.Control type="textarea" name="email" className="w-100 text-start px-0 ps-3" onChange={handleChange} />
+                            </Row>
+                            <Row className="mt-3">
+                                <Form.Label className="w-100 text-start px-0">Status ID</Form.Label>
+                                <Form.Control type="textarea" name="status" className="w-100 text-start px-0 ps-3" onChange={handleChange} />
+                            </Row>
+
+                            <Row className="mt-3">
+                                <Form.Label className="w-100 text-start px-0">Priorytet</Form.Label>
+                                <Form.Control type="number" min="1" name="priority" className="w-100 text-start px-0 ps-3" onChange={handleChange} />
+                            </Row>
+                            <Row className="mt-3">
+                                <Form.Label className="w-100 text-start px-0">Estymata</Form.Label>
+                                <Form.Control type="number" min="1" name="estimate" className="w-100 text-start px-0 ps-3" onChange={handleChange} />
+                            </Row>
+
                             <Row>
                                 <Col className="my-3 d-flex justify-content-center justify-content-md-center align-items-stretch px-0">
                                     <Button type="submit" className="w-100 px-0">Dodaj nową karte</Button>
